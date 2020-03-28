8063ba983abc733bbbc8964d5466796fde1aa3c0
An Nguyen <anthailan1702@gmail.com>
2020-03-28 16:41:17 +1100

    Add leet code exercises

---
+++ exercises/fruit_into_baskets.py
@@ -0,0 +1,42 @@
+"""
+Leet Code 904
+"""
+import unittest
+
+
+# Use sliding windows technique
+def total_fruit(tree):
+    lower = 0
+    upper = 0
+    basket = set()
+    max_fruits = 0
+
+    while upper < len(tree):
+        if tree[upper] not in basket and len(basket) == 2:
+            # Add this fruit and the one before it to the basket
+            lower = upper - 1
+            basket = {tree[lower], tree[upper]}
+
+            # Check if we have a chain of fruits in the back that are similar to the "lower" fruit
+            for i in range(lower-1, -1, -1):
+                if tree[i] != tree[lower]:
+                    break
+                else:
+                    lower = i
+        else:
+            basket.add(tree[upper])
+            upper += 1
+            max_fruits = max(max_fruits, upper - lower)
+    return max_fruits
+
+
+class TestResult(unittest.TestCase):
+
+    def test_one_tree(self):
+        self.assertEqual(total_fruit([1]), 1)
+
+    def test_long_sequence(self):
+        self.assertEqual(total_fruit([3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4]), 5)
+
+    def test_medium_sequence(self):
+        self.assertEqual(total_fruit([0, 1, 6, 6, 4, 4, 6]), 5)
---
+++ exercises/license_key_formatting.py
@@ -0,0 +1,50 @@
+"""
+Leet Code 482
+"""
+import unittest
+
+
+def license_key_formatting(string, k):
+    if not string:
+        return ''
+    string_builder = []
+    chars_in_group = 0
+    for i in range(len(string)-1, -1, -1):
+        if string[i] != '-':
+            string_builder.append(string[i].upper())
+            chars_in_group += 1
+            if chars_in_group == k:
+                string_builder.append('-')
+                chars_in_group = 0
+
+    if len(string_builder) == 0:
+        return ''
+
+    if string_builder[-1] == '-':
+        string_builder.pop()
+
+    return ''.join(list(reversed(string_builder)))
+
+
+class TestResult(unittest.TestCase):
+
+    def test_empty_string(self):
+        self.assertEqual(license_key_formatting('', 5), '')
+
+    def test_k_larger_than_string_length(self):
+        self.assertEqual(license_key_formatting('abcd', 5), 'ABCD')
+
+    def test_single_group(self):
+        self.assertEqual(license_key_formatting('a-bcd-efg', 7), 'ABCDEFG')
+
+    def test_first_group_has_less_characters(self):
+        self.assertEqual(license_key_formatting('a-b-cd-ef-g-h', 3), 'AB-CDE-FGH')
+
+    def test_all_group_equal_characters(self):
+        self.assertEqual(license_key_formatting('a-b-cd-ef-g-h-i', 3), 'ABC-DEF-GHI')
+
+    def test_dash_only_string(self):
+        self.assertEqual(license_key_formatting('---', 3), '')
+
+    def test_multiple_dashes_in_beginning(self):
+        self.assertEqual(license_key_formatting('---AA-AA', 2), 'AA-AA')
